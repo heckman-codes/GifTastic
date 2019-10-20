0d5fb5873c1895a4becd886ea53feae06e0b1b78
heckman-codes <liamheckmancode@gmail.com>
2019-10-20 16:42:15 -0700

    have images finally tiling correctly.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
     var offset = 0;
-    var offsetValue = parseInt($("#limiter").val());
+    var offsetValue = $("#limiter").val();
 
     $("#search-button").on("click", function (event) {
+        clearGifs();
         event.preventDefault();
         newSearch();
     });
@@ -15,23 +16,47 @@ $(document).ready(function () {
 
     $("#more-button").on("click", function (event) {
         event.preventDefault();
+        offsetValue = parseInt($("#limiter").val());
         offset = offset + offsetValue;
         console.log(offset + " | " + offsetValue);
-        newSearch();
+        $("#gif-display").append(newSearch());
+    });
+
+    $("#more-button").on("click", function (event) {
+        event.preventDefault();
+        offsetValue = parseInt($("#limiter").val());
+        offset = offset + offsetValue;
+        console.log(offset + " | " + offsetValue);
+        $("#gif-display").append(newSearch());
+    });
+
+    $(".grid-item").on("click", function () {
+        console.log($(this).attr("src", results[i].images.fixed_width.url));
+        // $(this).attr("src", results[i].images.fixed_width.url);
+    });
+
+
+    $("#limiter").find("option").on("click", function () {
+        offsetValue = $(this).val();
+        console.log(offsetValue);
     });
 
     function clearGifs() {
         offset = 0;
-        $("#gif-display").empty();
+        $(".grid").empty();
+        console.log(offset);
+
     }
 
+
     function newSearch() {
-        var searchTerm = $("#search-bar").val().trim();
+
         var limit = $("#limiter").val()
+        var searchTerm = $("#search-bar").val().trim();
         var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=1hTXSjTTporGV2mXgChe2f7BRzGA737n&q=" + searchTerm + "&limit=" + limit + "&offset=" + offset;
 
         console.log(queryURL);
-
+        console.log(offset);
 
         $.ajax({
             url: queryURL,
@@ -39,17 +64,15 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             var results = response.data;
-            clearGifs();
             for (let i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
+                var gifDiv = $("<div>").addClass("grid-item");
+                var gifInfo = $("<div>").addClass("div-info")
                 var gifImg = $("<img>");
-                $(gifDiv).addClass("img-holder");
-                $(".img-holder").css("float", "left");
-                $(".img-holder").css("margin", "0");
-                $(".img-holder").css("display", "inline-block");
-                $(gifImg).attr("src", results[i].images.fixed_height.url);
+                $(".grid").css({ "line-height": "0" });
+                $(".grid-item").css("float", "left");
+                $(gifImg).attr("src", results[i].images.original_still.url);
                 $(gifDiv).append(gifImg);
-                $("#gif-display").append(gifDiv);
+                $(".grid").append(gifDiv);
             }
         });
 
