154b2fd4eb3852de5da4be87d1136380e71569f4
heckman-codes <liamheckmancode@gmail.com>
2019-10-20 19:13:17 -0700

    have fully working Gif layout now. Need to add buttons and favorites section.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    var gifIndex = []
     var offset = 0;
     var offsetValue = $("#limiter").val();
 
@@ -14,25 +14,13 @@ $(document).ready(function () {
         clearGifs();
     });
 
-    $("#more-button").on("click", function (event) {
-        event.preventDefault();
-        offsetValue = parseInt($("#limiter").val());
-        offset = offset + offsetValue;
-        console.log(offset + " | " + offsetValue);
-        $("#gif-display").append(newSearch());
-    });
 
     $("#more-button").on("click", function (event) {
         event.preventDefault();
         offsetValue = parseInt($("#limiter").val());
         offset = offset + offsetValue;
         console.log(offset + " | " + offsetValue);
-        $("#gif-display").append(newSearch());
-    });
-
-    $(".grid-item").on("click", function () {
-        console.log($(this).attr("src", results[i].images.fixed_width.url));
-        // $(this).attr("src", results[i].images.fixed_width.url);
+        $(".grid").prepend(newSearch());
     });
 
 
@@ -43,14 +31,16 @@ $(document).ready(function () {
 
     function clearGifs() {
         offset = 0;
+        gifIndex = [];
         $(".grid").empty();
-        console.log(offset);
+        console.log(gifIndex);
 
     }
 
 
     function newSearch() {
 
+        gifIndex = [];
         var limit = $("#limiter").val()
         var searchTerm = $("#search-bar").val().trim();
         var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=1hTXSjTTporGV2mXgChe2f7BRzGA737n&q=" + searchTerm + "&limit=" + limit + "&offset=" + offset;
@@ -65,15 +55,29 @@ $(document).ready(function () {
             console.log(response);
             var results = response.data;
             for (let i = 0; i < results.length; i++) {
+                gifIndex.push(response.data[i]);
                 var gifDiv = $("<div>").addClass("grid-item");
-                var gifInfo = $("<div>").addClass("div-info")
+                var gifInfo = $("<span>").addClass("div-info")
                 var gifImg = $("<img>");
-                $(".grid").css({ "line-height": "0" });
-                $(".grid-item").css("float", "left");
+                $(".div-info").css("display", "initial");
+                $(gifImg).attr("data-gif", results[i].images.original.url);
                 $(gifImg).attr("src", results[i].images.original_still.url);
                 $(gifDiv).append(gifImg);
-                $(".grid").append(gifDiv);
+                $(gifDiv).append(gifInfo);
+                $(gifInfo).prepend("<p> Title: " + results[i].title + "</p><p> Rated: " + results[i].rating + "</p><p> By User: " + results[i].username + "</p>");
+                $(".grid").prepend(gifDiv);
             }
+            $("img").on("click", function () {
+                console.log(this);
+                var gif = $(this).attr("data-gif");
+                $(this).attr("src", gif)
+            });
+
+            $(".grid-item").hover(function () {
+                var gif = $(this).attr("data-gif");
+                $(this).find("img").attr("src", gif);
+                $(this).find(".div-info").toggle("display");
+            });
         });
 
     }
